c7dbadacebda40aa991981c2af1d0fd287d1ebeb
WilliamCarlos <masteratsoccer@gmail.com>
2017-08-18 23:05:59 -0700

    Changed frame <-> telem log file matching algorithm

--- frameSplittingScript/newScript.py
+++ frameSplittingScript/newScript.py
@@ -2,6 +2,7 @@ import cv2
 import pandas as pd
 import numpy as np
 import pdb
+import newScript_utils
 
 '''
 NOTE/TODO
@@ -13,9 +14,11 @@ junk above it
 
 '''
 Get video file from user use cv2 to: 
-    1.) Write frames as jpgs in ./images/
+    1.) Write frames as jpgs in ./frames
     2.) Get the number of frames in the video (stored in count)
 '''
+
+fps = 29.97 # TODO: take this as user input
 pathToVideo = input("Enter Path to Video: ");
 print("Exporting Frames from: ", pathToVideo)
 print("\nFrames written: ")
@@ -41,7 +44,6 @@ Status.
     - imageMatch is a mystery
     - continue to investigate this tmrw 
 '''
-pdb.set_trace()
 
 # USER IO AND DATA CLEANING
 telemetryInputPath = input("Enter Path to Telemetry Input File: ");
@@ -62,33 +64,10 @@ match = input("Enter the elapsed start time: "); # Why is this called match
 telemetryInput_pandas = pd.read_csv(telemetryInputPath, header=None)
 telemetryInput = telemetryInput_pandas.values
 
-
-# Compute the deltaTime array. Stores the elapsed time between each telemetry log entry.
-# deltaTime[index] is the time difference between index and index+1
-deltaTime = []
-prevRow = []
-for i, row in enumerate(telemetryInput):
-    if i == 0:
-        prevRow = row
-        continue;
-
-    dTime = row[0] - prevRow[0]
-    deltaTime.append(dTime)
-    prevRow = row
-    
-# Using the time between each log, compute how many frames were drawn in that time.
-# Assign all of those frames in the range [currentTime, currentTime + deltaTime[log+1]]
-# to the previous telemetry file. 
-fps = 29.97 #TODO: change this to read from Dalton's code later.
-framesPerBlock = []
-for i, dTime in enumerate(deltaTime):
-    frames = deltaTime[i] / (1/fps)  
-    framesPerBlock.append(frames)
-
-# Round frames to int. TODO. Must replace with roundingError.py when it is finished.
-roundedFramesPerBlock = framesPerBlock[:]
-for i in range(0, len(roundedFramesPerBlock)):
-    roundedFramesPerBlock[i] = int(round(roundedFramesPerBlock[i], 0))
+logFileTimes = newScript_utils.getLogFileTimes(telemetryInput)
+logFileTimes = newScript_utils.scaleLogFileTimes(logFileTimes, count, fps)
+frameTimes = newScript_utils.getFrameTimes(count, fps)
+alignedPairs = newScript_utils.alignLogAndFrames(frameTimes, logFileTimes, count)
 
 # need some of Dalton's code here:
 image = ""
@@ -98,20 +77,17 @@ col = [0] * 4 # List with 4 0's
 
 j = 0
 currentBlockNum = 0
-for i,lines in enumerate(telemetryInputFile):    
-    col[1] = telemetryInput[currentBlockNum][1]
-    col[2] = telemetryInput[currentBlockNum][2]
-    col[3] = telemetryInput[currentBlockNum][3]
-    numFramesInCurrentBlock = roundedFramesPerBlock[currentBlockNum] # this doesn't need to be computed every time todo
+for i,lines in enumerate(telemetryInputFile):
+    col[1] = telemetryInput[alignedPairs[i]][1]
+    col[2] = telemetryInput[alignedPairs[i]][2]
+    col[3] = telemetryInput[alignedPairs[i]][3]
+    print(i)
+
     imageNum = str(imageNum)
     image = "frame-" + imageNum + imageType
     telemetryOutputFile.write(image + " {0} {1} {2}\n".format(str(col[1]), str(col[2]), str(col[3])))
     imageNum = int(imageNum) + 1
     j += 1
-
-    if j == numFramesInCurrentBlock:
-        currentBlockNum += 1
-        j = 0
 
 
     '''
