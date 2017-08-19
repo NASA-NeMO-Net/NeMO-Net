6a1731e5dd9f054b5f40fa6756f45654956d0a18
WilliamCarlos <masteratsoccer@gmail.com>
2017-08-19 15:52:13 -0700

    Refactored variable names.

--- frameSplittingScript/newScript.py
+++ frameSplittingScript/newScript.py
@@ -15,7 +15,7 @@ junk above it
 '''
 Get video file from user use cv2 to: 
     1.) Write frames as jpgs in ./frames
-    2.) Get the number of frames in the video (stored in count)
+    2.) Get the number of frames in the video (stored in frameCount)
 '''
 
 fps = 29.97 # TODO: take this as user input
@@ -24,22 +24,22 @@ print("Exporting Frames from: ", pathToVideo)
 print("\nFrames written: ")
 
 vidcap = cv2.VideoCapture(pathToVideo)
-count = 0
+frameCount = 0
 
 success, frame = vidcap.read()
 while success:
-  cv2.imwrite("frames/frame-%d.jpg" % count, frame)
+  cv2.imwrite("frames/frame-%d.jpg" % frameCount, frame)
   success, frame = vidcap.read()
-  if count % 25 == 0:
-    print(count)
-  count += 1
+  if frameCount % 25 == 0:
+    print(frameCount)
+  frameCount += 1
 
-print("Number of frames generated: ", count)
-imageMatch = "frame-"+str(count)+".jpg" # TODO: what is imageMatch
+print("Number of frames generated: ", frameCount)
+imageMatch = "frame-"+str(frameCount)+".jpg" # TODO: what is imageMatch
 
 '''
 Status. 
-    - count has the total number of frames
+    - frameCount has the total number of frames
     - imageMatch is a mystery
     - continue to investigate this tmrw 
 '''
@@ -50,14 +50,14 @@ telemetryOutputPath = input("Enter Path to Telemetry Output File: ");
 
 telemetryInputFile = open(telemetryInputPath, "r")
 telemetryOutputFile = open(telemetryOutputPath, "w")
-# match = input("Enter the elapsed start time: "); # Why is this called match
-match = 82.804
+droneStartTime = input("Enter the elapsed drone start time: "); 
+droneStartTime = 82.804
 
 # This might be useful later
 #found = False
 #while found != True: #Loop searches for the elapsed time you want to start export at
 #    col = telemetryInputFile.readline().split()
-#    if col[0] == match:
+#    if col[0] == droneStartTime:
 #        found = True
 
 
@@ -66,9 +66,9 @@ telemetryInput_pandas = pd.read_csv(telemetryInputPath, header=None)
 telemetryInput = telemetryInput_pandas.values
 
 logFileTimes = newScript_utils.getLogFileTimes(telemetryInput)
-logFileTimes = newScript_utils.scaleLogFileTimes(logFileTimes, count, fps)
-frameTimes = newScript_utils.getFrameTimes(count, fps)
-alignedPairs = newScript_utils.alignLogAndFrames(frameTimes, logFileTimes, count)
+logFileTimes = newScript_utils.scaleLogFileTimes(logFileTimes, frameCount, fps)
+frameTimes = newScript_utils.getFrameTimes(frameCount, fps)
+alignedPairs = newScript_utils.alignLogAndFrames(frameTimes, logFileTimes, frameCount)
 
 # need some of Dalton's code here:
 image = ""
@@ -78,7 +78,7 @@ col = [0] * 4 # List with 4 0's
 
 j = 0
 currentBlockNum = 0
-for i in range(0, count):
+for i in range(0, frameCount):
     # pdb.set_trace()
     col[1] = telemetryInput[alignedPairs[i]][1]
     col[2] = telemetryInput[alignedPairs[i]][2]
@@ -95,7 +95,7 @@ for i in range(0, count):
     '''
     Better way: delete image, imageMatch Replace with: 
     
-    if i+1 == count:    
+    if i+1 == frameCount:    
     '''  
     if image == imageMatch:
         print("<Final Frame Reached>")
