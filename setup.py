e06f8fd5a415bf623f96b37ad167e4043571e5f9
Alan Li <rechantizer@gmail.com>
2020-12-21 16:30:35 -0800

    Creating setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+	name = 'NASA NeMO-Net', 
+	version = '0.1',
+	description = 'Training and classification of Satellite Coral Imagery',
+	author = 'Alan Li',
+	packages=["nemo-net"],
+	install_requires=[
+		'numpy',
+		'opencv-python',
+		'matplotlib',
+		'jupyter',
+		'scikit-learn',
+		'pandas',
+		'Pillow',
+		'GDAL',
+		'pydensecrf',
+		'tensorflow-gpu==1.5.0'
+		'keras==2.0.8',
+	],
+)
